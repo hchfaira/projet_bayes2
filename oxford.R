2c10b34a519b6e1610b1b4f2c2634b3ff317d055
Jean-Charles G <jeancharles.girard85@gmail.com>
2021-04-01 20:50:40 +0200

    quantile+correctio

--- oxford.R
+++ oxford.R
@@ -21,14 +21,10 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
   for (iter in 1:nchain){
     current <- chain[iter,]
     
-    cat("iter")
-    cat(iter)
-    
     ##Mise à jour sigma2
     update_shape <- 10^(-3) + K/2
     update_scale <- 10^(-3) + sum(current[4:(3+K)]^2)/2
-    sigma2 <- rinvgamma(1, shape=update_shape, scale=update_scale)
-    current[4+2*K] <- sigma2
+    current[4+2*K] <- rinvgamma(1, shape=update_shape, scale=update_scale)
     
     ## Mise a jour de alpha
     prop <- current
@@ -42,7 +38,7 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
       acc_rates[1] <- acc_rates[1] + 1
     }
     
-    ## Mise a jour de bata1
+    ## Mise a jour de beta1
     prop <- current
     prop[2] <- rnorm(1, current[2], prop_sd[2])
     
@@ -56,7 +52,7 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
     }
     
     
-    ## Mise a jour de bata2
+    ## Mise a jour de beta2
     prop <- current
     prop[3] <- rnorm(1, current[3], prop_sd[3])
     
@@ -104,9 +100,13 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
     return(list(chain = chain, acc_rates = acc_rates / nchain))
   }
   
-  ## Application
-  K <- 120
-  r1 <-
+
+#################
+## Application ##
+################
+
+K <- 120
+r1 <-
     c(3, 5, 2, 7, 7, 2, 5, 3, 5, 11, 6, 6, 11, 4, 4, 2, 8, 8, 6, 
       5, 15, 4, 9, 9, 4, 12, 8, 8, 6, 8, 12, 4, 7, 16, 12, 9, 4, 7, 
       8, 11, 5, 12, 8, 17, 9, 3, 2, 7, 6, 5, 11, 14, 13, 8, 6, 4, 8, 
@@ -114,7 +114,7 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
       9, 11, 6, 14, 21, 16, 6, 9, 8, 9, 8, 4, 11, 11, 6, 9, 4, 4, 9, 
       9, 10, 14, 6, 3, 4, 6, 10, 4, 3, 3, 10, 4, 10, 5, 4, 3, 13, 1, 
       7, 5, 7, 6, 3, 7)
-  n1 <-
+n1 <-
     c(28, 21, 32, 35, 35, 38, 30, 43, 49, 53, 31, 35, 46, 53, 61, 
       40, 29, 44, 52, 55, 61, 31, 48, 44, 42, 53, 56, 71, 43, 43, 43, 
       40, 44, 70, 75, 71, 37, 31, 42, 46, 47, 55, 63, 91, 43, 39, 35, 
@@ -123,14 +123,14 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
       63, 71, 69, 43, 50, 31, 34, 54, 46, 58, 62, 52, 41, 34, 52, 63, 
       59, 88, 62, 47, 53, 57, 74, 68, 61, 45, 45, 62, 73, 53, 39, 45, 
       51, 55, 41, 53, 51, 42, 46, 54, 32)
-  r0 <-
+r0 <-
     c(0, 2, 2, 1, 2, 0, 1, 1, 1, 2, 4, 4, 2, 1, 7, 4, 3, 5, 3, 2, 
       4, 1, 4, 5, 2, 7, 5, 8, 2, 3, 5, 4, 1, 6, 5, 11, 5, 2, 5, 8, 
       5, 6, 6, 10, 7, 5, 5, 2, 8, 1, 13, 9, 11, 9, 4, 4, 8, 6, 8, 6, 
       8, 14, 6, 5, 5, 2, 4, 2, 9, 5, 6, 7, 5, 10, 3, 2, 1, 7, 9, 13, 
       9, 11, 4, 8, 2, 3, 7, 4, 7, 5, 6, 6, 5, 6, 9, 7, 7, 7, 4, 2, 
       3, 4, 10, 3, 4, 2, 10, 5, 4, 5, 4, 6, 5, 3, 2, 2, 4, 6, 4, 1)
-  n0 <-
+n0 <-
     c(28, 21, 32, 35, 35, 38, 30, 43, 49, 53, 31, 35, 46, 53, 61, 
       40, 29, 44, 52, 55, 61, 31, 48, 44, 42, 53, 56, 71, 43, 43, 43, 
       40, 44, 70, 75, 71, 37, 31, 42, 46, 47, 55, 63, 91, 43, 39, 35, 
@@ -139,7 +139,7 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
       63, 71, 69, 43, 50, 31, 34, 54, 46, 58, 62, 52, 41, 34, 52, 63, 
       59, 88, 62, 47, 53, 57, 74, 68, 61, 45, 45, 62, 73, 53, 39, 45, 
       51, 55, 41, 53, 51, 42, 46, 54, 32)
-  year<-
+year<-
     c(-10, -9, -9, -8, -8, -8, -7, -7, -7, -7, -6, -6, -6, -6, -6, 
       -5, -5, -5, -5, -5, -5, -4, -4, -4, -4, -4, -4, -4, -3, -3, -3, 
       -3, -3, -3, -3, -3, -2, -2, -2, -2, -2, -2, -2, -2, -2, -1, -1, 
@@ -148,27 +148,42 @@ oxford <- function(year, n1, n0, r1, r0, nchain = 10^4, prop_sd = c(0.05, 0.03, 
       3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 
       6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 10)
   
-  out <- oxford(year, n1, n0, r1, r0)
-  out$chain <- out$chain[-(1:1000),]
-  text=c("alpha", "beta1", "beta2", "sigma2")
-  par(mfrow = c(4, 2), mar = c(4, 5, 0.5, 0.5))
-  for (j in 1:3){
-    plot(out$chain[,j], type = "l", main = "",ylab=text[j])
-    plot(density(out$chain[,j]), type = "l", main = "")
-  }
+ out <- oxford(year, n1, n0, r1, r0)
+  
+#burning period
+out$chain <- out$chain[-(1:1000),]
+  
+text=c("alpha", "beta1", "beta2", "sigma2")
+par(mfrow = c(4, 2), mar = c(4, 5, 0.5, 0.5))
+for (j in 1:3){
+  plot(out$chain[,j], type = "l", main = "",ylab=text[j])
+  plot(density(out$chain[,j]), type = "l", main = "")
+}
     
-  plot(out$chain[,244], type = "l", main = "",ylab=text[4])
-  plot(density(out$chain[,244]), type = "l", main = "")
+plot(out$chain[,244], type = "l", main = "",ylab=text[4])
+plot(density(out$chain[,244]), type = "l", main = "")
   
+#Moyenne
+moy_alpha <- mean(out$chain[8000:9001,1])
+moy_beta1 <- mean(out$chain[8000:9001,2])
+moy_beta2 <- mean(out$chain[8000:9001,3])
+moy_sigma <- mean(sqrt(out$chain[8000:9001,244]))
   
-  moy_alpha <- mean(out$chain[8000:9001,1])
-  moy_beta1 <- mean(out$chain[8000:9001,2])
-  moy_beta2 <- mean(out$chain[8000:9001,3])
-  moy_sigma2 <- mean(out$chain[8000:9001,244])
-  
+#Écart-type
 sd_alpha <- sqrt(mean(out$chain[8000:9001,1]^2)-mean(out$chain[8000:9001,1])^2)
 sd_beta1 <- sqrt(mean(out$chain[8000:9001,2]^2)-mean(out$chain[8000:9001,2])^2)
 sd_beta2 <- sqrt(mean(out$chain[8000:9001,3]^2)-mean(out$chain[8000:9001,3])^2)
-sd_sigma2 <- sqrt(mean(out$chain[8000:9001,4]^2)-mean(out$chain[8000:9001,4])^2)
+sd_sigma <- sqrt(mean(sqrt(out$chain[8000:9001,244])^2)-mean(sqrt(out$chain[8000:9001,244]))^2)
+
+#Intervalles de confiance à 95%
+quantile(out$chain[8000:9001,1], prob=c(0.025,0.975))
+quantile(out$chain[8000:9001,2], prob=c(0.025,0.975))
+quantile(out$chain[8000:9001,3], prob=c(0.025,0.975))
+quantile(sqrt(out$chain[8000:9001,244]), prob=c(0.025,0.975))
+
+#ACF
 acf(out$chain[,c(1,2,3,244)],type=c('correlation'))
 acf
+
+
+
